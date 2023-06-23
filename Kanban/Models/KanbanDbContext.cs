5467265b2427cd2eb9a89a18ad9ed4bfc118ff31
Jeroen Serdijn <jeroen_serdijn@hotmail.com>
2023-06-23 14:13:02 +0200

    Added everything mentioned in feature branch name.

---
+++ Kanban/Models/KanbanDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kanban.Models
+{
+    public class KanbanDbContext :DbContext
+    {
+        public KanbanDbContext(DbContextOptions<KanbanDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public  DbSet<Board> Boards { get; set; }
+        public DbSet<Section> Sections { get; set; }
+        public DbSet<Card> Cards { get; set; }
+    }
+}
---
+++ Kanban/Models/Section.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Kanban.Models
+{
+    public class Section
+    {
+
+        [Key] 
+        public int SectionId { get; set; }
+
+        public int SectionOrderIndex { get; set; }
+
+        public int SectionCreatedById { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public required string SectionTitle { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string? SectionDescription { get; set; }
+    }
+}
